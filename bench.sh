ed42d2f7c80b629c650acc7d81e7b3f47a5661a3
danglotb <bdanglot@gmail.com>
2015-09-20 13:12:42 +0200

    add script to bench

---
+++ bench.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+scala -cp bin main.MainHillClimbing -select first -neighbor exchange -init rnd -k 30 -file input/wt100.txt
+scala -cp bin main.MainHillClimbing -select first -neighbor exchange -init mdd -k 30 -file input/wt100.txt
+scala -cp bin main.MainHillClimbing -select first -neighbor exchange -init edd -k 30 -file input/wt100.txt
+#scala -cp bin main.MainHillClimbing -select first -neighbor swap -init rnd -k 30 -file input/wt100.txt
+#scala -cp bin main.MainHillClimbing -select first -neighbor swap -init mdd -k 30 -file input/wt100.txt
+#scala -cp bin main.MainHillClimbing -select first -neighbor swap -init edd -k 30 -file input/wt100.txt
+#scala -cp bin main.MainHillClimbing -select first -neighbor insert -init rnd -k 30 -file input/wt100.txt
+#scala -cp bin main.MainHillClimbing -select first -neighbor insert -init mdd -k 30 -file input/wt100.txt
+#scala -cp bin main.MainHillClimbing -select first -neighbor insert -init edd -k 30 -file input/wt100.txt
+scala -cp bin main.MainHillClimbing -select best -neighbor exchange -init rnd -k 30 -file input/wt100.txt
+scala -cp bin main.MainHillClimbing -select best -neighbor exchange -init mdd -k 30 -file input/wt100.txt
+scala -cp bin main.MainHillClimbing -select best -neighbor exchange -init edd -k 30 -file input/wt100.txt
+#scala -cp bin main.MainHillClimbing -select best -neighbor swap -init rnd -k 30 -file input/wt100.txt
+#scala -cp bin main.MainHillClimbing -select best -neighbor swap -init mdd -k 30 -file input/wt100.txt
+#scala -cp bin main.MainHillClimbing -select best -neighbor swap -init edd -k 30 -file input/wt100.txt
+#scala -cp bin main.MainHillClimbing -select best -neighbor insert -init rnd -k 30 -file input/wt100.txt
+#scala -cp bin main.MainHillClimbing -select best -neighbor insert -init mdd -k 30 -file input/wt100.txt
+#scala -cp bin main.MainHillClimbing -select best -neighbor insert -init edd -k 30 -file input/wt100.txt
