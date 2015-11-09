7a43dd358a6e66772bfaadab6dcbd1e905650d21
danglobt <bdanglot@gmail.com>
2015-11-09 13:21:07 +0100

    filterMax done

--- src/solver/multiobjectiv/Filter.scala
+++ src/solver/multiobjectiv/Filter.scala
@@ -11,18 +11,16 @@ object Filter2o {
 
   def filter(solution: List[(Int, Int)], max: Boolean = false): List[(Int, Int)] = {
     if (max)
-      filterMax(solution)
+      filterMax(solution, 0)
     else
       filterMin(solution, 0)
   }
 
   private def filterMin(solution: List[(Int, Int)],  i: Int): List[(Int, Int)] = {
-    println("solution\t"+solution)
     if (i >= solution.length)
       solution
     else {
       val dominated = solution.filter { p => compare2Tuple(p,solution(i))}
-      println("dominated\t"+dominated)
       if (dominated.length == 0)
         filterMin(solution.filter(p => p != solution(i)), 0)
       else
@@ -30,8 +28,16 @@ object Filter2o {
     }
   }
 
-  private def filterMax(solution: List[(Int, Int)]): List[(Int, Int)] = {
-    solution
+  private def filterMax(solution: List[(Int, Int)], i : Int): List[(Int, Int)] = {
+    if (i >= solution.length)
+      solution
+    else {
+      val dominated = solution.filter { p => compare2Tuple(solution(i), p)}
+      if (dominated.length == 0)
+        filterMax(solution.filter(p => p != solution(i)), 0)
+      else
+        filterMax(solution.diff(dominated), i+1)   
+    }
   }
 
   private def compare2Tuple(t1: (Int, Int), t2: (Int, Int)) = t1._1 > t2._1 && t1._2 > t2._2
