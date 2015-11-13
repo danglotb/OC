e77e431b2e61661e0362c19fe225a7c8128ed30b
danglobt <bdanglot@gmail.com>
2015-11-13 12:52:26 +0100

    split in 2 objects

--- src/main/multiobjectiv/MainRandom.scala
+++ src/main/multiobjectiv/MainRandom.scala
@@ -4,9 +4,8 @@ import data.multiobjectiv._
 import solver.multiobjectiv._
 import data.Logger
 
-object MainRandom extends App {
-
-  def build500RandomOnLine(solutions: List[(Int, Int)], dominate: List[(Int, Int)], instance: InstanceTSP, i : Int): (List[(Int, Int)], List[(Int, Int)]) = {
+object Build500Random {
+   def build500RandomOnLine(solutions: List[(Int, Int)], dominate: List[(Int, Int)], instance: InstanceTSP, i : Int): (List[(Int, Int)], List[(Int, Int)]) = {
     if (i == 500)
       (solutions,dominate)
     else {
@@ -25,19 +24,16 @@ object MainRandom extends App {
       build500Random(solutions :+ (newSolution(0), newSolution(1)), instance)
     }
   }
+}
+
+object MainOnLineRandom extends App {
 
   val instance = tspBuilder.build2mTsp("input/tsp/randomA100.tsp", "input/tsp/randomB100.tsp")
 
-  //  val solutions = build500Random(Nil, instance)
-
-  val solutions = build500RandomOnLine(Nil, Nil,instance,0)
+  val solutions = Build500Random.build500RandomOnLine(Nil, Nil,instance,0)
 
   //build data
   var str = ""
-  //  Filter2o.filter(solutions).foreach { x =>
-  //    str += x._1 + "\t" + x._2 + "\n"
-  //  }
-
   solutions._1.foreach { x =>
     str += x._1 + "\t" + x._2 + "\n"
   }
@@ -45,6 +41,32 @@ object MainRandom extends App {
 
   str = ""
   solutions._2.foreach { x =>
+    str += x._1 + "\t" + x._2 + "\n"
+  }
+  data.Util.write("data/random500mTSP.dat", str)
+
+  //build plt file
+  str = "plot \"data/random500mTSP.dat\" title \'non-dominate \',  \"data/domRandom500mTSP.dat\"title \'dominate\'"
+  data.Util.write("plot/random500mTSP.plt", str)
+}
+
+object MainOffLineRandom extends App {
+
+ 
+  val instance = tspBuilder.build2mTsp("input/tsp/randomA100.tsp", "input/tsp/randomB100.tsp")
+
+    val solutions = Build500Random.build500Random(Nil, instance)
+
+  //build data
+  var str = ""
+  val dominators =  Filter2o.filter(solutions)
+  dominators.foreach { x =>
+      str += x._1 + "\t" + x._2 + "\n"
+  }
+  data.Util.write("data/domRandom500mTSP.dat", str)
+
+  str = ""
+  (solutions.diff(dominators)).foreach { x =>
     str += x._1 + "\t" + x._2 + "\n"
   }
   data.Util.write("data/random500mTSP.dat", str)
